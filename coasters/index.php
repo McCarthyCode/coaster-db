c6aa45aa0882a75105871388bc27290064ab2e2f
Matt McCarthy <mattmc318@gmail.com>
2016-06-25 20:16:44 -0500

    Removed superfluous CSS from coasters/index.php

--- coasters/index.php
+++ coasters/index.php
@@ -3,24 +3,6 @@
 <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 <body>
-<style>
-table {
-        border-collapse: collapse;
-            width: 100%;
-}
-
-th, td {
-        text-align: left;
-            padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-th {
-        background-color: #4CAF50;
-            color: white;
-}
-</style>
 </head>
 <body>
 <?php
