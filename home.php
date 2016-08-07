1b81f21ff723d17216a52a66941b5aa04c5cb0ec
Matt McCarthy <mattmc318@gmail.com>
2016-08-06 20:30:24 -0500

    Allows users to logout from homepage.

--- home.php
+++ home.php
@@ -1,9 +1,20 @@
 <?php
 session_start();
-if(isset($_SESSION['username'])) {
-echo "Hello, " . $_SESSION['username'] . "!<br/>";
-} else {
+if(!isset($_SESSION['username'])) {
     header("Location: index.php");
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div>
+<?php
+echo "Hello, " . $_SESSION['username'] . "!<br/>";
+?>
+<a href="logout.php">logout</a>
+</div>
+<body>
+</html>
 
